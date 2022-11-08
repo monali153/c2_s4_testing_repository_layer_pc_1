f9cb20af543249e22f731af1deb4c8a441aae1c3
Monali Sharma <mona.chaudhari444@gmail.com>
2022-11-08 18:37:14 +0530

    pc complete

---
+++ practice1/src/main/java/com/example/s3practice1/practice1/service/CustomerService.java
@@ -0,0 +1,14 @@
+package com.example.s3practice1.practice1.service;
+
+import com.example.s3practice1.practice1.domain.Customer;
+import com.example.s3practice1.practice1.exception.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer addCustomerDetails(Customer customer);
+    List<Customer> getAllCustomer() throws CustomerNotFoundException;
+    boolean deleteCustomerById(int customerId) throws CustomerNotFoundException;
+    List<Customer> getAllCustomerByProductSamsung(String productName) throws CustomerNotFoundException;
+}
